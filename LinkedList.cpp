ba29a0113767731e4ef8f8331ed40735138fc358
cass1618 <cass16180@gmail.com>
2024-10-12 22:30:07 -0700

    :sparkles: add methods to populate list with random integers and display list

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -12,12 +12,29 @@ LinkedList::~LinkedList()
 
 }
 
+// build a list of random integers
 void LinkedList::build_list()
 {
-
+    srand (time(NULL));
+    int number_of_nodes = rand() % 20;
+    if (number_of_nodes < 8) number_of_nodes +=8;
+    for (int i = 0; i <= number_of_nodes; ++i) 
+    {
+        node* new_node = new node();
+        new_node->data = rand() % 103;
+        new_node->next = head;
+        head = new_node;
+    }
 }
 
+// display list of integers stored in linked list
 void LinkedList::display_list()
 {
-
+    node* current = head;
+    while(current)
+    {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl << endl;
 }
--- main.cpp
+++ main.cpp
@@ -4,5 +4,9 @@ using namespace std;
 
 int main()
 {
+    LinkedList* list = new LinkedList();
+    list->build_list();
+    list->display_list();
+
     return 0;
 }
